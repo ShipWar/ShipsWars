ed3cdf3573269f96375e363dd9092b48bfdbc664
PANCERZ <tpancerz2@gmail.com>
2017-04-29 15:40:50 +0200

    add SFML library and libs for execute aplication on windows

--- ShipWars.pro
+++ ShipWars.pro
@@ -5,8 +5,8 @@ CONFIG -= qt
 
 LIBS += -LC:/SFML/lib
 
-CONFIG(release, debug|release): LIBS += -lsfml-audio -lsfml-graphics -lsfml-main -lsfml-network -lsfml-window -lsfml-system
-CONFIG(debug, debug|release): LIBS += -lsfml-audio-d -lsfml-graphics-d -lsfml-main-d -lsfml-network-d -lsfml-window-d -lsfml-system-d
+CONFIG(release, debug|release): LIBS += -lsfml-graphics -lsfml-main -lsfml-system
+CONFIG(debug, debug|release): LIBS += -lsfml-graphics-d -lsfml-main-d -lsfml-system-d
 
 INCLUDEPATH += C:/SFML/include
 DEPENDPATH += C:/SFML/include
--- main.cpp
+++ main.cpp
@@ -5,6 +5,26 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+
+    sf::Texture texture;
+     if (!texture.loadFromFile("D:/userdata/pancerz/Desktop/GameProject/ShipWars/images/space.jpg"))
+         return EXIT_FAILURE;
+     sf::Sprite sprite(texture);
+
+    while (window.isOpen())
+    {
+       // Process events
+       sf::Event event;
+       while (window.pollEvent(event))
+       {
+           // Close window : exit
+           if (event.type == sf::Event::Closed)
+               window.close();
+       }
+       window.draw(sprite);
+       window.display();
+    }
     return 0;
 }
